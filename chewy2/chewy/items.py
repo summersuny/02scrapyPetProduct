885729b572151926cda9d7be8b2b7f4e7a735298
summersuny <sunsai777@gmail.com>
2017-08-02 11:17:40 -0400

    scrapy code added

---
+++ chewy2/chewy/items.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ChewyItem(scrapy.Item):
+    # define the fields for your item here like:
+    category = scrapy.Field()
+    name = scrapy.Field()
+    brand = scrapy.Field()
+    rating = scrapy.Field()
+    reviews_number = scrapy.Field()
+    price = scrapy.Field()
+    unit = scrapy.Field()
+    ingredient = scrapy.Field()
+    page = scrapy.Field()
+    
---
+++ chewy2/chewy/spiders/chewy_spider.py
@@ -0,0 +1,67 @@
+from chewy.items import ChewyItem
+from scrapy import Spider, Request
+
+class ChewySpider(Spider):
+	name='chewy_spider'
+	allowed_urls=['https://www.chewy.com']	
+	start_urls=['https://www.chewy.com/b/food-387'] 
+	index = 1
+
+	def parse(self,response):
+		print ("Crawling page "+str(self.index))
+		
+		links=response.xpath('.//article[@class="product-holder  cw-card cw-card-hover"]/a/@href').extract()
+		for link in links:
+			url='https://www.chewy.com'+link
+			request = Request(url,callback=self.parse_detail)
+			request.meta['page'] = self.index
+			yield request
+
+		self.index = self.index + 1
+
+		next_link = response.xpath('.//li[@class="next "]/a/@href').extract_first()
+		if type(next_link) == type(''):
+			url='https://www.chewy.com'+next_link
+			yield Request(url,callback=self.parse)
+		else:
+			print("waiting for pending tasks")
+
+
+	def parse_detail(self,response):
+		category=response.xpath('.//nav[@class="breadcrumbs container"]//li/a/text()').extract()
+		name=response.xpath('.//div[@id="product-title"]/h1/text()').extract_first()
+		brand=response.xpath('.//div[@id="brand"]/a/text()').extract_first()
+		rating=response.xpath('.//div[@class="ugc ugc-head"]//img/@src').extract_first()
+		reviews_number=response.xpath('.//div[@class="ugc ugc-head"]//span/text()').extract_first()
+		price=response.xpath('.//span[@class="ga-eec__price"]/text()').extract_first()
+		unit=response.xpath('.//span[@class="attribute-selection__value js-selection-value"]/text()').extract_first()
+		ingredient=response.xpath('.//div[@class="cw-bullets"]//tbody//td/text()').extract()
+
+		
+		item=ChewyItem()
+		#if type(price) == type(None):
+		#	print("response:")
+		#	print(response)
+		#	print(response.body)
+
+		item['category']=category
+		item['name']=name
+		item['brand']=brand
+		item['rating']=rating
+		item['reviews_number']=reviews_number
+		item['price']=price
+		item['unit']=unit
+		item['ingredient']=ingredient
+		item['page'] = response.meta['page']
+
+		yield item	
+
+		
+		
+
+
+	
+	
+
+
+
